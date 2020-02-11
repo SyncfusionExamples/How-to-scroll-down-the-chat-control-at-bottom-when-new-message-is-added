19861ccf6cade659d23214fb2c02baf89cb78009
Karthik Raja <Karthikraja.kalaimani@syncfusion.com>
2020-02-11 14:39:20 +0530

    Sample added and committed

--- SfChat/GettingStarted/MainPage.xaml.cs
+++ SfChat/GettingStarted/MainPage.xaml.cs
@@ -1,35 +1,4 @@
-# How-to-scroll-down-the-chat-control-at-bottom-when-new-message-is-added
-
-##About the sample
-
-This repository contains sample that demonstrates how to scroll chat messages by programmatically. 
-
-```XAML
-<ContentPage xmlns="http://xamarin.com/schemas/2014/forms"
-             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
-             xmlns:sfChat="clr-namespace:Syncfusion.XForms.Chat;assembly=Syncfusion.SfChat.XForms"
-             xmlns:local="clr-namespace:GettingStarted"
-             x:Class="GettingStarted.MainPage">
-
-    <ContentPage.BindingContext>
-        <local:GettingStattedViewModel x:Name="viewModel"/>
-    </ContentPage.BindingContext>
-    <ContentPage.Behaviors>
-        <local:MainPageBehavior></local:MainPageBehavior>
-    </ContentPage.Behaviors>
-    
-    <ContentPage.Content>
-        <sfChat:SfChat x:Name="sfChat"
-                       Messages="{Binding Messages}"
-                       SendMessageCommand="{Binding SendMessage}"
-                       CurrentUser="{Binding CurrentUser}" />
-    </ContentPage.Content>
-
-</ContentPage>
-
-```c#
-
-using Syncfusion.XForms.Chat;
+﻿using Syncfusion.XForms.Chat;
 using System.Collections.Specialized;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -96,12 +65,3 @@ namespace GettingStarted
 
     }
 }
-
-## <a name="requirements-to-run-the-demo"></a>Requirements to run the demo ##
-
-* [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/) or [Visual Studio for Mac](https://visualstudio.microsoft.com/vs/mac/).
-* Xamarin add-ons for Visual Studio (available via the Visual Studio installer).
-
-## <a name="troubleshooting"></a>Troubleshooting ##
-### Path too long exception
-If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
